1369cb46823744ba2f41cfd839306f590729bb6e
Jazlene Rae Guevarra <43012576+jguevarra@users.noreply.github.com>
2022-12-07 19:08:29 -0500

    set max idle connections limit to 0 (#83)

--- stores/store-utilities.go
+++ stores/store-utilities.go
@@ -19,9 +19,7 @@ func InitStores(appConfig *config.AppConfig) (*PallidSturgeonStore, error) {
 		"godror",
 		"user="+appConfig.Dbuser+" password="+appConfig.Dbpass+" connectString="+connectString+" poolMaxSessions=100 poolSessionMaxLifetime=2m0s",
 	)
-	// db.SetMaxIdleConns(2)
-	// db.SetConnMaxLifetime(2 * time.Minute)
-	// db.SetMaxOpenConns(100)
+	db.SetMaxIdleConns(0)
 	if err != nil {
 		log.Printf("[InitStores] m=GetDb,msg=connection has failed: %s", err)
 		return nil, err
