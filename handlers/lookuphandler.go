85c79972cb205d780b5c22da504b3c21a1c6540e
Jazlene Rae Guevarra <43012576+jguevarra@users.noreply.github.com>
2026-04-16 12:19:05 -0400

    bug: CWBI-28873: Frequency ID Lookup data model, queries, and endpoint (#134)

---
+++ handlers/lookuphandler.go
@@ -0,0 +1,28 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/USACE/pallid_sturgeon_api/server/models"
+	"github.com/USACE/pallid_sturgeon_api/server/stores"
+	"github.com/labstack/echo/v4"
+)
+
+type LookupHandler struct {
+	Store *stores.LookupStore
+}
+
+func (s *LookupHandler) GetAllLookups(c echo.Context) error {
+	// GLOBAL LOOK UP QUERIES
+	frequencyIds, err := s.Store.GetFrequencyIds()
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, models.NewErrorResponse("Failed to retrieve Frequency ID data", err))
+	}
+
+	// Single combined response
+	response := map[string]any{
+		"frequencyIds":    frequencyIds,
+	}
+
+	return c.JSON(http.StatusOK, models.NewSuccessResponse("Lookup data retrieved successfully", response))
+}
